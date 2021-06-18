e53c1ec63cc988b32b5117c362919b5de1f91544
Jovin Kumar S <44001189+jovindrc@users.noreply.github.com>
2021-06-18 21:42:56 +0530

    Add files via upload

---
+++ Multiple Inputs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Multiple inputs with Python
+
+# In[ ]:
+
+
+while True:
+    reply =input("Enter Text:")
+    if reply == 'stop':
+        break
+    print(reply)
+        
+
+
+# In[ ]:
+
+
+
+
